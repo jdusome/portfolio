e7059180a662b1f201952ad3a295944835acaab9
jdusome <200236108@student.georgianc.on.ca>
2016-10-03 16:53:19 -0400

    added basic content to each page of application

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('home', { title: 'Home'
+  });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About Me'
+  });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects'
+  });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Services'
+  });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me'
+  });
 });
 
 module.exports = router;
